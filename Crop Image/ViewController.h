b92c3235631be0ed4e6aa681421bd3112cdd13bc
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-10-29 12:09:39 +0530

    Initial Commit

---
+++ Crop Image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crop Image
+//
+//  Created by paradigm creatives on 10/29/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Crop Image/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Crop Image
+//
+//  Created by paradigm creatives on 10/29/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
